f7b594050bb97fd7a3527dde062512f1d598c52a
Martin Raiber <martin@urbackup.org>
2012-05-16 21:59:17 +0200

    Incremented version

--- build_msi.bat
+++ build_msi.bat
@@ -1,3 +1,3 @@
 candle -ext WixUtilExtension -ext WixFirewallExtension urbackup.wxs
-light urbackup.wixobj -ext WixUtilExtension -ext WixFirewallExtension -ext WixUIExtension -out "UrBackup Client 0.40-1(x64).msi"
+light urbackup.wixobj -ext WixUtilExtension -ext WixFirewallExtension -ext WixUIExtension -out "UrBackup Client 1.0-1(x64).msi"
 pause
