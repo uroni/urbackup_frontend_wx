bff4e80802c271a36590d723f065399f42914c04
Martin <martin@urbackup.org>
2016-04-25 17:49:45 +0200

    Build with signing
    (cherry picked from commit 716e925562dbd910901e7cc901150af76340a13c)

--- build_client.bat
+++ build_client.bat
@@ -10,6 +10,12 @@ if %errorlevel% neq 0 exit /b %errorlevel%
 
 call update_data.bat
 
+if NOT "%SIGN%" == "true" GOTO skip_signing1
+
+signtool sign /fd sha256 /t http://timestamp.digicert.com /i DigiCert data\*.exe data\*.dll data_x64\*.exe data_x64\*.dll
+
+:skip_signing1
+
 "C:\Program Files (x86)\NSIS\Unicode\makensis.exe" "%~dp0/urbackup.nsi"
 if %errorlevel% neq 0 exit /b %errorlevel%
 
@@ -20,3 +26,9 @@ if %errorlevel% neq 0 exit /b %errorlevel%
 if %errorlevel% neq 0 exit /b %errorlevel%
 
 call build_msi.bat
+
+if NOT "%SIGN%" == "true" GOTO skip_signing2
+
+signtool sign /fd sha256 /t http://timestamp.digicert.com /i DigiCert "UrBackup Client $version_short$.exe" "UrBackup Client NoTray $version_short$.exe" "UrBackup Client $version_short$(x64).msi" "UrBackup Client (No tray) $version_short$(x64).msi"
+
+:skip_signing2
