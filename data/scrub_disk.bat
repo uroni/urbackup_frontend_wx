386959abfb3364029f58910f361ecd90416a42d0
Martin <martin@urbackup.org>
2019-04-26 11:32:30 +0200

    Check for admin permissions

--- data/scrub_disk.bat
+++ data/scrub_disk.bat
@@ -1,5 +1,12 @@
 @echo off
 
+net session >nul 2>&1
+if NOT %errorLevel% == 0 (
+	echo Failure: Current permissions inadequate. Please run as administrator.
+	pause
+	exit /b 1
+)
+
 set scrubDisks="%1"
 if [%1]==[] set scrubDisks="SelectViaGUI"
 
