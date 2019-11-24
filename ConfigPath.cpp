688d9e8264d643342d1dd688a835f82b06cd5a8f
Martin <martin@urbackup.org>
2019-11-24 15:11:24 +0100

    Fix build

--- ConfigPath.cpp
+++ ConfigPath.cpp
@@ -98,7 +98,8 @@ ConfigPath::ConfigPath(wxWindow* parent)
 		if (dir.path.find("|") != std::string::npos)
 		{
 			std::vector<std::string> toks;
-			TokenizeMail(std::string(dir.path.ToUTF8()), toks, "|");
+			std::string s_dir(dir.path.ToUTF8());
+			TokenizeMail(s_dir, toks, "|");
 			dir.path = toks[0];
 			dir.name = toks[1];
 			if (toks.size()>2)
