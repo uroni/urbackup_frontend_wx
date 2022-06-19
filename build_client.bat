6bccb18ffead4d92e506df417dc56fabd2528a44
Martin <martin@urbackup.org>
2022-06-19 11:07:16 +0200

    Change certificate
    (cherry picked from commit fd3719d5855e8014b9d9a230c357b8b1712c6aa1)
    
    # Conflicts:
    #	build_client.bat

--- build_client.bat
+++ build_client.bat
@@ -12,7 +12,7 @@ call update_data.bat
 
 if NOT "%SIGN%" == "true" GOTO skip_signing1
 
-signtool sign /fd sha256 /t http://timestamp.digicert.com /i DigiCert data\*.exe data\*.dll data_x64\*.exe data_x64\*.dll
+signtool sign /fd sha256 /t http://time.certum.pl /i Certum data\*.exe data\*.dll data_x64\*.exe data_x64\*.dll
 
 :skip_signing1
 
@@ -29,6 +29,6 @@ call build_msi.bat
 
 if NOT "%SIGN%" == "true" GOTO skip_signing2
 
-signtool sign /fd sha256 /t http://timestamp.digicert.com /i DigiCert "UrBackup Client $version_short$.exe" "UrBackup Client NoTray $version_short$.exe" "UrBackup Client $version_short$(x64).msi" "UrBackup Client (No tray) $version_short$(x64).msi"
+signtool sign /fd sha256 /t http://time.certum.pl /i Certum "UrBackup Client $version_short$.exe" "UrBackup Client NoTray $version_short$.exe" "UrBackup Client $version_short$(x64).msi" "UrBackup Client (No tray) $version_short$(x64).msi"
 
 :skip_signing2
