527c4323b46e9a92dad9b679641661bc0b858420
Martin Raiber <martin@urbackup.org>
2026-05-24 00:17:26 +0200

    Remove vcpkg triplet

--- build_client.bat
+++ build_client.bat
@@ -2,13 +2,13 @@ cd %~dp0
 
 call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsamd64_x86.bat"
 
-msbuild UrBackupClientGUI.sln /p:Configuration=Release /p:Platform="Win32" /p:vcpkgTriplet="x86-windows-static-md"
+msbuild UrBackupClientGUI.sln /p:Configuration=Release /p:Platform="Win32"
 if %errorlevel% neq 0 exit /b %errorlevel% 
 
-msbuild UrBackupClientGUI.sln /p:Configuration=Release /p:Platform="x64" /p:vcpkgTriplet="x64-windows-static-md"
+msbuild UrBackupClientGUI.sln /p:Configuration=Release /p:Platform="x64"
 if %errorlevel% neq 0 exit /b %errorlevel%
 
-msbuild UrBackupClientGUI.sln /p:Configuration=Release /p:Platform="ARM64" /p:vcpkgTriplet="arm64-windows-static-md"
+msbuild UrBackupClientGUI.sln /p:Configuration=Release /p:Platform="ARM64"
 if %errorlevel% neq 0 exit /b %errorlevel%
 
 call update_data.bat
